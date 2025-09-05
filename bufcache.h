b6be2b7e682150273b174c0e3a3f040d172775e8
Huzaifa <huzaifakhan3339@gmail.com>
2025-09-05 15:15:41 -0500

    getblk + bread compile tested only

--- bufcache.h
+++ bufcache.h
@@ -12,8 +12,8 @@ struct BCache * initialize_cache();
 void print_hash_queue(struct BCache *bc);
 Buffer * search_hq(int block_num, struct BCache *bc);
 
-Buffer getblk(int const blk_num);
-int brelse(int const buf_num);
-int bread(int const blk_num, int read_ahead_count);
-int bwrite(Buffer *);
-
+Buffer * getblk(int const blk_num, struct BCache *bc);
+int brelse(int const blk_num, struct BCache *bc);
+Buffer * bread(int const blk_num, struct BCache *bc);
+Buffer * breada(int const blk_num, int read_ahead_count, struct BCache *bc);
+int bwrite(Buffer * buf, struct BCache *bc);
